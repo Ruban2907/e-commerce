36fa5e15543d6ec1dc0826dfc164ce6cce9bbabf
Ruban2907 <rubanahmed29@gmail.com>
2025-07-31 20:35:25 +0500

    items backed implementation

--- e-commerce/controller/auth.js
+++ e-commerce/controller/auth.js
@@ -7,7 +7,7 @@ const multer = require("../middleware/multer");
 async function handleUserlogin(req,res) {
     const {email, password} = req.body;
     if (!email || !password)
-    {
+    {  
        return res.status(400).json({ message: "Email and Password are required" });
     }
     const foundUser = await User.findOne({ email: email });
